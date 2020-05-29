161367d495fe87129bf3d7d73a251941ba2996ca
rohan bansal <rohan2003bansal@gmail.com>
2020-05-29 03:47:22 -0500

    Updated website to work with proper funcationality on the backend and run recursively to allow for embedding changes.

--- docs/master.js
+++ docs/master.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $(this).attr('target', '_blank');
 });
   $("button").click(function() {
-    $("table").remove();
+    $("#rank_results").remove();
     $("#spinner").show();
     var a = $("#s1").val();
     var b = $("#s2").val();
@@ -31,8 +31,46 @@ $(document).ready(function() {
   let ranked_articles = await fetch(Url, payLoad)
   .then(response => response.json())
   .then(data => {return data});
-
-
+  var grand_html = ""
+  ranked_articles.forEach(function(article) {
+    grand_html += "<tr>";
+    grand_html += '<td class="title mdl-data-table__cell--non-numeric"><a class="article_link" href="';
+    grand_html += article['link'];
+    grand_html += '">';
+    grand_html += article['title'];
+    grand_html += '<br></br><br></br>';
+    grand_html += '<p class="top_words">';
+    article['top_words'].forEach(function(word) {
+      if (word.length > 2 && isNaN(word)) {
+        grand_html += word;
+        grand_html += ",";
+      };
+    });
+    grand_html = grand_html.slice(0, -1)
+    grand_html += '</p>';
+    grand_html += '<p class="least_words">';
+    article['least_words'].forEach(function(word) {
+      if (word.length > 2 && isNaN(word)) {
+        grand_html += word;
+        grand_html += ",";
+      };
+    });
+    grand_html = grand_html.slice(0, -1)
+    grand_html += '</p>';
+    grand_html += '</td>';
+    grand_html += '<td class="logit">';
+    grand_html += article['logit'].toString();
+    grand_html += '</td>'
+    grand_html += '</tr>'
+  });
+  grand_html += '</tbody>'
+  grand_html += '</table>'
+  var prepend = '<table id="rank_results" style="width:90%; margin-left:5%; margin-right:5%;" class="mdl-data-table mdl-js-data-table">'
+  prepend += "<thead>"
+  prepend += '<tr><th class="mdl-data-table__cell--non-numeric">Articles</th><th class="mdl-data-table__cell--non-numeric">Prediction</th></tr></thead><tbody>'
+  var final_html_str = prepend + grand_html
+  $("#spinner").hide();
+  $("#table-wrapper").append(final_html_str)
 })()
 });
 });
