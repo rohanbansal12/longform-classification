713d60a3a1947dd2a74ef3826615822dcffc8b6a
rohan bansal <rohan2003bansal@gmail.com>
2020-07-02 00:37:49 -0500

    Add BERT approach with first 512 tokens used for predictions. Correct mapping issue with tokens.

---
+++ BERT/arguments/train_arguments.py
@@ -0,0 +1,105 @@
+import os
+import argparse
+import pathlib
+
+
+def expand_path(string):
+    return pathlib.Path(os.path.expandvars(string))
+
+
+def str2bool(v):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+def add_data(parser):
+    parser.add_argument(
+        "--train_path", type=expand_path, required=True, help="Path to train set"
+    )
+    parser.add_argument(
+        "--test_path", type=expand_path, required=True, help="Path to test set"
+    )
+    parser.add_argument(
+        "--eval_path", type=expand_path, required=True, help="Path to evaluation set"
+    )
+    parser.add_argument(
+        "--data_dir",
+        type=expand_path,
+        required=False,
+        help="Path for data storage and loading. Only required if dictionaries and mapped-data being created.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=expand_path,
+        required=True,
+        help="Directory for storing results",
+    )
+    parser.add_argument(
+        "--create_dicts",
+        type=str2bool,
+        default=True,
+        help="Create dictionaries for articles, publications, and words.",
+    )
+    parser.add_argument(
+        "--dict_dir",
+        type=expand_path,
+        help="Path where dictionaries are located. Required if --create_dict is False.",
+    )
+    parser.add_argument(
+        "--map_items",
+        type=str2bool,
+        default=True,
+        help="True if data must be mapped to ids, else False",
+    )
+    parser.add_argument(
+        "--tokenize",
+        type=str2bool,
+        default=True,
+        help="Tokenize and split data fields.",
+    )
+    parser.add_argument(
+        "--target_publication",
+        type=int,
+        default=0,
+        help="Designate target publication.",
+    )
+
+
+def add_training(parser):
+    parser.add_argument(
+        "--batch_size", type=int, default=64, help="Batch size for training"
+    )
+    parser.add_argument(
+        "--use_gpu",
+        type=str2bool,
+        default=True,
+        help="Use GPU for training and evaluation",
+    )
+    parser.add_argument(
+        "--training_steps",
+        type=int,
+        default=1000,
+        help="Total Number Of Steps For Training",
+    )
+    parser.add_argument(
+        "--frequency",
+        type=int,
+        default=1000,
+        help="How often to calculate recall and test performance.",
+    )
+    parser.add_argument(
+        "--recall_max", type=int, default=150, help="What to calculate recall out of."
+    )
+
+
+def add_optimization(parser):
+    parser.add_argument(
+        "--learning_rate", type=float, default=1e-4, help="Set optimizer learning rate"
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0.9, help="Set optimizer momentum."
+    )
---
+++ BERT/gridsearch/run-bert-train.py
@@ -0,0 +1,59 @@
+import copy
+import jobs
+import pathlib
+import addict
+
+
+def get_slurm_script_gpu(output_dir, command):
+    """Returns contents of SLURM script for a gpu job."""
+    return """#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-socket=1
+#SBATCH --gres=gpu:tesla_p100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64000
+#SBATCH --output={}/slurm_%j.out
+#SBATCH -t 05:59:00
+#module load anaconda3 cudatoolkit/10.0 cudnn/cuda-10.0/7.3.1
+#source activate yumi
+{}
+""".format(
+        output_dir, command
+    )
+
+
+if __name__ == "__main__":
+    commands = [
+        "PYTHONPATH=. python train-rankfromsets.py  --train_path /scratch/gpfs/altosaar/dat/longform-data/rankfromsets/combined-data/train.json --test_path /scratch/gpfs/altosaar/dat/longform-data/rankfromsets/combined-data/test.json --eval_path /scratch/gpfs/altosaar/dat/longform-data/rankfromsets/combined-data/evaluation.json "
+    ]
+
+    experiment_name = "news-inner-product"
+    log_dir = (
+        pathlib.Path(pathlib.os.environ["LOG"]) / "news-classification-inner-product"
+    )
+
+    base_grid = addict.Dict()
+    base_grid.create_dicts = False
+    base_grid.map_items = False
+    base_grid.recall_max = 1000
+    base_grid.tokenize = False
+    base_grid.target_publication = 0
+    base_grid.batch_size = 2000
+    base_grid.training_steps = 1500
+    base_grid.use_gpu = True
+    base_grid.frequency = 100
+    base_grid.dict_dir = pathlib.Path(
+        "/scratch/gpfs/altosaar/dat/longform-data/rankfromsets/dictionaries"
+    )
+
+    # RMS with all words
+    grid = copy.deepcopy(base_grid)
+    grid["learning_rate"] = [2e-5, 3e-5, 4e-5, 5e-5]
+    keys_for_dir_name = jobs.get_keys_for_dir_name(grid)
+    for cfg in jobs.param_grid(grid):
+        cfg["output_dir"] = jobs.make_output_dir(
+            log_dir, experiment_name, cfg, keys_for_dir_name
+        )
+        jobs.submit(commands, cfg, get_slurm_script_gpu)
+
