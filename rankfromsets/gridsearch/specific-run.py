2e4a4e2f2d3835808e4d02502bcdf9c11c008da6
rohanbansal12 <rohan2003bansal@gmail.com>
2020-07-22 00:23:05 -0500

    Update specific-run.py

--- rankfromsets/gridsearch/specific-run.py
+++ rankfromsets/gridsearch/specific-run.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         "PYTHONPATH=. python train-rankfromsets.py  --train_path /scratch/gpfs/altosaar/dat/longform-data/main/combined-data/train.json --test_path /scratch/gpfs/altosaar/dat/longform-data/main/combined-data/test.json --eval_path /scratch/gpfs/altosaar/dat/longform-data/main/combined-data/evaluation.json "
     ]
 
-    experiment_name = "news-BERT"
+    experiment_name = "news-rankfromsets"
     log_dir = (
         pathlib.Path(pathlib.os.environ["LOG"]) / "news-classification-inner-product"
     )
