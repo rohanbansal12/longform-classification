758ddcc91c616037b115ff08cafaa5e5f332adea
rohanbansal12 <rohan2003bansal@gmail.com>
2020-07-28 14:16:34 -0500

    Clean and Update demo

--- demo/final-demo/html-generation.py
+++ demo/final-demo/html-generation.py
@@ -35,11 +35,14 @@ parser.add_argument(
     "--data_matrix_path",
     type=expand_path,
     required=True,
-    help="This is required to load dictionaries",
+    help="This is required to load data matrix",
 )
 
 parser.add_argument(
-    "--dict_dir", type=expand_path, required=True, help="Path to data to be ranked."
+    "--dict_dir",
+    type=expand_path,
+    required=True,
+    help="Path to location of dictionaries",
 )
 
 parser.add_argument(
@@ -75,110 +78,20 @@ print("Dictionaries loaded.")
 # load numeric data for calculations
 publication_emb = np.asarray(
     [
-        1.293169,
-        0.5968991,
-        0.08624366,
-        -1.340886,
-        0.20788613,
-        -1.8755758,
-        -1.048286,
-        -0.89399487,
-        -0.6858106,
-        -0.5503035,
-        -0.7068237,
-        -0.07774379,
-        -0.5711354,
-        -0.06891353,
-        -0.6157936,
-        0.18361174,
-        1.0387663,
-        -0.39592394,
-        -0.01400398,
-        0.93364245,
-        0.31408283,
-        -1.5882636,
-        -1.1210498,
-        1.3615427,
-        0.04305666,
-        -0.25838053,
-        0.40041822,
-        0.64693785,
-        1.355679,
-        -0.3234386,
-        -0.28630123,
-        -1.2223343,
-        -0.5902191,
-        0.31748208,
-        0.6941454,
-        0.54294777,
-        0.61911935,
-        -0.50641966,
-        -1.1085368,
-        -0.06808531,
-        -1.9941399,
-        1.4491764,
-        0.13361451,
-        -0.65689725,
-        1.430887,
-        -0.03157316,
-        -0.10601741,
-        1.3829597,
-        0.18713853,
-        -0.7091327,
-        -1.0947673,
-        0.33397934,
-        -0.6568065,
-        0.72214764,
-        0.07785704,
-        0.39983407,
-        1.9645799,
-        -2.0063865,
-        -0.03951475,
-        -1.0200714,
-        1.6216166,
-        -0.32680988,
-        1.2766384,
-        0.56323916,
-        0.7573127,
-        0.02096812,
-        0.8105754,
-        -0.07347149,
-        -0.17383198,
-        0.78832185,
-        1.3038499,
-        0.9610421,
-        0.9688594,
-        0.45888966,
-        -0.87990636,
-        1.397912,
-        -0.87064624,
-        0.4131552,
-        1.201137,
-        0.13622381,
-        -1.6119416,
-        -0.892891,
-        -1.022667,
-        0.25875703,
-        -0.43832338,
-        -0.8434626,
-        1.0337343,
-        2.521364,
-        -2.3373847,
-        2.627967,
-        -0.5229926,
-        0.13273215,
-        -1.1643656,
-        -1.2185771,
-        0.06260775,
-        0.24288402,
-        -1.0325723,
-        -1.638396,
-        -1.7829332,
-        0.23202017,
+        0.77765566,
+        0.76451594,
+        0.75550663,
+        -0.7732487,
+        0.7341457,
+        0.7216135,
+        -0.7263404,
+        0.73897207,
+        -0.720818,
+        0.73908365,
     ],
     dtype=np.float32,
 )
-publication_bias = 3
+publication_bias = 0.43974462
 
 word_article_path = args.data_matrix_path
 word_articles = scipy.sparse.load_npz(word_article_path)
@@ -193,7 +106,9 @@ print(word_articles.shape)
 print(word_emb.shape)
 article_embeddings = word_articles.dot(word_emb)
 
-emb_times_publication = np.dot(article_embeddings, publication_emb.reshape(100, 1))
+emb_times_publication = np.dot(
+    article_embeddings, publication_emb.reshape(len(publication_emb), 1)
+)
 
 article_bias = word_articles.dot(word_bias)
 
@@ -205,7 +120,9 @@ final_logits = np.divide(product_with_bias, word_counts) + float(publication_bia
 
 indices = final_logits.argsort(axis=0)[-75:].reshape(75)
 
-word_logits = np.dot(word_emb, publication_emb.reshape(100, 1)) + word_bias
+word_logits = (
+    np.dot(word_emb, publication_emb.reshape(len(publication_emb), 1)) + word_bias
+)
 
 top_articles = word_articles[indices.tolist()[0]]
 
@@ -227,11 +144,21 @@ for idx in indices.tolist()[0]:
     least_words = []
     for top_word in current_sorted_words[-20:]:
         word = id_to_word[top_word]
-        if "unused" not in word and "##" not in word and len(word) > 1:
+        if (
+            "unused" not in word
+            and "##" not in word
+            and len(word) > 1
+            and "[UNK]" not in word
+        ):
             top_words.append(word)
     for least_word in current_sorted_words[:20]:
         word = id_to_word[least_word]
-        if "unused" not in word and "##" not in word and len(word) > 1:
+        if (
+            "unused" not in word
+            and "##" not in word
+            and len(word) > 1
+            and "[UNK]" not in word
+        ):
             least_words.append(word)
     current_article["top_words"] = top_words
     current_article["least_words"] = least_words
